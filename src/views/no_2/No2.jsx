031eea42f62078db23dba5f9014be12c04070ca3
dessert99 <lsjh1234@naver.com>
2025-05-26 17:27:48 +0900

    No2 토글 기능 완성

--- src/views/no_2/No2.jsx
+++ src/views/no_2/No2.jsx
@@ -11,23 +11,15 @@ const No2 = () => {
           <h2>두 번째 기능입니다</h2>
           <LinkButton to="/">홈으로 가기</LinkButton>
         </div>
-        <h3>리액트로 다양한 방법으로 토글 기능을 만들어 보자.</h3>
+        <h3>리액트로 토글 기능을 만들어 보자.</h3>
         <hr />
       </div>
       <div className="no2_main">
         <div className="toggle1">
-          <p>1. useState를 사용하여 토글 기능 만들기</p>
           <No2useState1 />
           <No2useState2 />
         </div>
         <hr />
-        <div className="toggle2">
-          <p>2. useReducer를 사용하여 토글 기능 만들기</p>
-        </div>
-        <hr />
-        <div className="toggle3">
-          <p>3. useToggle 커스텀 훅을 사용하여 토글 기능 만들기</p>
-        </div>
       </div>
     </>
   );
