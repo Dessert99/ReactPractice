f5ce575f41abb4602e28d603e7e42c35fe655a91
Dessert99 <lsjh1234@naver.com>
2025-09-06 23:58:30 +0900

    MUI설치

--- src/App.js
+++ src/App.js
@@ -8,24 +8,24 @@ import No4 from './pages/No_4/No4';
 import No5 from './pages/No_5/No5';
 
 //헤더
-import Header from './components/header/Header';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/No1" element={<No1 />} />
-          <Route path="/No2" element={<No2 />} />
-          <Route path="/No3" element={<No3 />} />
-          <Route path="/No4" element={<No4 />} />
-          <Route path="/No5" element={<No5 />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/No1" element={<No1 />} />
+                    <Route path="/No2" element={<No2 />} />
+                    <Route path="/No3" element={<No3 />} />
+                    <Route path="/No4" element={<No4 />} />
+                    <Route path="/No5" element={<No5 />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,25 +1,29 @@
 import LinkButton from '../../components/LinkButton';
 import styled from 'styled-components';
+import { Button } from '@mui/material';
+import Accordion from '@mui/material';
+
 const Container = styled.div`
-  padding: 1rem;
+    padding: 1rem;
 `;
 const Wrapper = styled.div`
-  display: flex;
-  gap: 1rem;
+    display: flex;
+    gap: 1rem;
 `;
 
 const MainPage = () => {
-  return (
-    <Container>
-      <Wrapper>
-        <LinkButton to="/No1">가로스크롤</LinkButton>
-        <LinkButton to="/No2">토글</LinkButton>
-        <LinkButton to="/No3">카카오맵</LinkButton>
-        <LinkButton to="/No4">AI챗봇</LinkButton>
-        <LinkButton to="/No5">파일 업로드</LinkButton>
-      </Wrapper>
-    </Container>
-  );
+    return (
+        <Container>
+            <Wrapper>
+                <LinkButton to="/No1">가로스크롤</LinkButton>
+                <LinkButton to="/No2">토글</LinkButton>
+                <LinkButton to="/No3">카카오맵</LinkButton>
+                <LinkButton to="/No4">AI챗봇</LinkButton>
+                <LinkButton to="/No5">파일 업로드</LinkButton>
+                <Button variant="contained">MUI연습 중이애요</Button>
+            </Wrapper>
+        </Container>
+    );
 };
 
 export default MainPage;
